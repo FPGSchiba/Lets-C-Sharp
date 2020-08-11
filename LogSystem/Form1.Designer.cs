02d94e5b7d6b2cf90c1273cb22783bbf29a80bc2
FPGSchiba <craftzockerlp@gmail.com>
2020-08-11 10:37:05 +0200

    Versuch gescheitert

--- LogSystem/Form1.Designer.cs
+++ LogSystem/Form1.Designer.cs
@@ -31,8 +31,6 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
@@ -59,34 +57,12 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "label2";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Enabled = false;
-            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label3.Location = new System.Drawing.Point(36, 92);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Enabled = false;
-            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(36, 134);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "label4";
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Enabled = false;
             this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label5.Location = new System.Drawing.Point(36, 176);
+            this.label5.Location = new System.Drawing.Point(36, 105);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(35, 13);
             this.label5.TabIndex = 4;
@@ -103,10 +79,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(326, 218);
+            this.ClientSize = new System.Drawing.Size(249, 140);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -121,8 +95,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Timer timer1;
     }
--- LogSystem/Form1.cs
+++ LogSystem/Form1.cs
@@ -18,9 +18,6 @@ namespace LogSystem
 
         PerformanceCounter perfCPUCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
         PerformanceCounter perfMEMCounter = new PerformanceCounter("Memory", "% Committed Bytes in Use");
-        PerformanceCounter perfSYSCounter = new PerformanceCounter("System", "System Up Time");
-        PerformanceCounter perfGPUCounter = new PerformanceCounter("GPU Adapter Memory", "Shared Usage", "luid_0x00000000_0x00013869_phys_0");
-        PerformanceCounter perfNETCounter = new PerformanceCounter("PhysicalDisk", "% Idle Time", "_Total");
 
         public Form1()
         {
@@ -31,9 +28,6 @@ namespace LogSystem
         {
             label1.Text = "CPU:" + "   " + perfCPUCounter.NextValue().ToString("F", CultureInfo.CreateSpecificCulture("en-US")) + "   " + "%";
             label2.Text = "MEM:" + "   " + perfMEMCounter.NextValue().ToString("F", CultureInfo.CreateSpecificCulture("en-US")) + "   " + "%";
-            label3.Text = "SYS Up Time:" + "   " + perfSYSCounter.NextValue().ToString("F", CultureInfo.CreateSpecificCulture("en-US")) + "   " + "Seconds";
-            label4.Text = "Disk Idle Time" + "   " + perfNETCounter.NextValue().ToString("F", CultureInfo.CreateSpecificCulture("en-US")) + "   " + "%";
-            label5.Text = "GPU:" + "   " + perfGPUCounter.NextValue().ToString("F", CultureInfo.CreateSpecificCulture("en-US")) + "   " + "%";
         }
     }
 }
