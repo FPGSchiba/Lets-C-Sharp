5514639c44513327a94efeb31f528cf9481f85a0
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-08-25 22:32:12 +0200

    Pferderennen erweitert
    Bitte Realtime Leaderboard erstellen

---
+++ Pferderennen/Pferderennen/Logger.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using MongoDB.Bson.IO;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pferderennen
+{
+    static class Logger
+    {
+        public static string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Pferderennen");
+        public static string infoPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Pferderennen\InfoLog.txt");
+        public static string errorPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), @"Pferderennen\ErroroLog.txt");
+        public static int linecountinfo = CountLinesReader(infoPath);
+        public static int linecountrerror = CountLinesReader(errorPath);
+
+        public static void ErrorLog(string Message)
+        {
+            createDir();
+
+            if (!File.Exists(errorPath))
+            {
+                File.Create(errorPath);
+            }
+
+            if (linecountrerror >= 10000)
+            {
+                File.WriteAllText(errorPath, "");
+            }
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(errorPath, true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + " - Error: " + Message);
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public static void InfoLog(string Message)
+        {
+            createDir();
+
+            if (!File.Exists(infoPath))
+            {
+                File.Create(infoPath);
+            }
+
+            if(linecountinfo >= 10000)
+            {
+                File.WriteAllText(infoPath, "");
+            }
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(infoPath, true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString() + " - Info: " + Message);
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private static void createDir()
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                DirectorySecurity directorySecurity = new DirectorySecurity();
+                FileSystemAccessRule rule1 = new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, AccessControlType.Allow);
+                FileSystemAccessRule rule2 = new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.LocalSystemSid, null), FileSystemRights.FullControl, AccessControlType.Allow);
+                directorySecurity.AddAccessRule(rule1);
+                directorySecurity.AddAccessRule(rule2);
+                Directory.CreateDirectory(folderPath, directorySecurity);
+            }
+        }
+
+        public static int CountLinesReader(string Path)
+        {
+            if (File.Exists(Path))
+            {
+                int lineCounter = 0;
+
+                using (var reader = new StreamReader(Path))
+                {
+
+                    while (reader.ReadLine() != null)
+                    {
+
+                        lineCounter++;
+
+                    }
+
+                    return lineCounter;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
--- Pferderennen/Pferderennen/Race.Designer.cs
+++ Pferderennen/Pferderennen/Race.Designer.cs
@@ -28,199 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.progressBar2 = new System.Windows.Forms.ProgressBar();
-            this.progressBar3 = new System.Windows.Forms.ProgressBar();
-            this.progressBar4 = new System.Windows.Forms.ProgressBar();
-            this.progressBar5 = new System.Windows.Forms.ProgressBar();
-            this.progressBar6 = new System.Windows.Forms.ProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.pB_Ruedi = new System.Windows.Forms.ProgressBar();
+            this.pB_Fredi = new System.Windows.Forms.ProgressBar();
+            this.pB_Hans = new System.Windows.Forms.ProgressBar();
+            this.pB_Peter = new System.Windows.Forms.ProgressBar();
+            this.pB_Fridolin = new System.Windows.Forms.ProgressBar();
+            this.pB_Bertha = new System.Windows.Forms.ProgressBar();
+            this.posRuedi = new System.Windows.Forms.Label();
+            this.posFredi = new System.Windows.Forms.Label();
+            this.posHans = new System.Windows.Forms.Label();
+            this.posPeter = new System.Windows.Forms.Label();
+            this.posFridolin = new System.Windows.Forms.Label();
+            this.posBertha = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // progressBar1
+            // pB_Ruedi
             // 
-            this.progressBar1.Location = new System.Drawing.Point(93, 34);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(591, 23);
-            this.progressBar1.TabIndex = 0;
+            this.pB_Ruedi.Location = new System.Drawing.Point(140, 52);
+            this.pB_Ruedi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pB_Ruedi.MarqueeAnimationSpeed = 10;
+            this.pB_Ruedi.Maximum = 10000;
+            this.pB_Ruedi.Name = "pB_Ruedi";
+            this.pB_Ruedi.Size = new System.Drawing.Size(886, 35);
+            this.pB_Ruedi.Step = 1;
+            this.pB_Ruedi.TabIndex = 0;
             // 
-            // progressBar2
+            // pB_Fredi
             // 
-            this.progressBar2.Location = new System.Drawing.Point(93, 94);
-            this.progressBar2.Name = "progressBar2";
-            this.progressBar2.Size = new System.Drawing.Size(591, 23);
-            this.progressBar2.TabIndex = 1;
+            this.pB_Fredi.Location = new System.Drawing.Point(140, 145);
+            this.pB_Fredi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pB_Fredi.MarqueeAnimationSpeed = 10;
+            this.pB_Fredi.Maximum = 10000;
+            this.pB_Fredi.Name = "pB_Fredi";
+            this.pB_Fredi.Size = new System.Drawing.Size(886, 35);
+            this.pB_Fredi.Step = 1;
+            this.pB_Fredi.TabIndex = 1;
             // 
-            // progressBar3
+            // pB_Hans
             // 
-            this.progressBar3.Location = new System.Drawing.Point(93, 149);
-            this.progressBar3.Name = "progressBar3";
-            this.progressBar3.Size = new System.Drawing.Size(591, 23);
-            this.progressBar3.TabIndex = 2;
+            this.pB_Hans.Location = new System.Drawing.Point(140, 229);
+            this.pB_Hans.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pB_Hans.MarqueeAnimationSpeed = 10;
+            this.pB_Hans.Maximum = 10000;
+            this.pB_Hans.Name = "pB_Hans";
+            this.pB_Hans.Size = new System.Drawing.Size(886, 35);
+            this.pB_Hans.Step = 1;
+            this.pB_Hans.TabIndex = 2;
             // 
-            // progressBar4
+            // pB_Peter
             // 
-            this.progressBar4.Location = new System.Drawing.Point(93, 208);
-            this.progressBar4.Name = "progressBar4";
-            this.progressBar4.Size = new System.Drawing.Size(591, 23);
-            this.progressBar4.TabIndex = 3;
+            this.pB_Peter.Location = new System.Drawing.Point(140, 320);
+            this.pB_Peter.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pB_Peter.MarqueeAnimationSpeed = 10;
+            this.pB_Peter.Maximum = 10000;
+            this.pB_Peter.Name = "pB_Peter";
+            this.pB_Peter.Size = new System.Drawing.Size(886, 35);
+            this.pB_Peter.Step = 1;
+            this.pB_Peter.TabIndex = 3;
             // 
-            // progressBar5
+            // pB_Fridolin
             // 
-            this.progressBar5.Location = new System.Drawing.Point(93, 260);
-            this.progressBar5.Name = "progressBar5";
-            this.progressBar5.Size = new System.Drawing.Size(591, 23);
-            this.progressBar5.TabIndex = 4;
+            this.pB_Fridolin.Location = new System.Drawing.Point(140, 400);
+            this.pB_Fridolin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pB_Fridolin.MarqueeAnimationSpeed = 10;
+            this.pB_Fridolin.Maximum = 10000;
+            this.pB_Fridolin.Name = "pB_Fridolin";
+            this.pB_Fridolin.Size = new System.Drawing.Size(886, 35);
+            this.pB_Fridolin.Step = 1;
+            this.pB_Fridolin.TabIndex = 4;
             // 
-            // progressBar6
+            // pB_Bertha
             // 
-            this.progressBar6.Location = new System.Drawing.Point(93, 318);
-            this.progressBar6.Name = "progressBar6";
-            this.progressBar6.Size = new System.Drawing.Size(591, 23);
-            this.progressBar6.TabIndex = 5;
+            this.pB_Bertha.Location = new System.Drawing.Point(140, 489);
+            this.pB_Bertha.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pB_Bertha.MarqueeAnimationSpeed = 10;
+            this.pB_Bertha.Maximum = 10000;
+            this.pB_Bertha.Name = "pB_Bertha";
+            this.pB_Bertha.Size = new System.Drawing.Size(886, 35);
+            this.pB_Bertha.Step = 1;
+            this.pB_Bertha.TabIndex = 5;
             // 
-            // label1
+            // posRuedi
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(690, 34);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Position:";
+            this.posRuedi.AutoSize = true;
+            this.posRuedi.Location = new System.Drawing.Point(1035, 52);
+            this.posRuedi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.posRuedi.Name = "posRuedi";
+            this.posRuedi.Size = new System.Drawing.Size(69, 20);
+            this.posRuedi.TabIndex = 6;
+            this.posRuedi.Text = "Position:";
             // 
-            // label2
+            // posFredi
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(690, 94);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(47, 13);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Position:";
+            this.posFredi.AutoSize = true;
+            this.posFredi.Location = new System.Drawing.Point(1035, 145);
+            this.posFredi.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.posFredi.Name = "posFredi";
+            this.posFredi.Size = new System.Drawing.Size(69, 20);
+            this.posFredi.TabIndex = 7;
+            this.posFredi.Text = "Position:";
             // 
-            // label3
+            // posHans
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(690, 149);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(47, 13);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Position:";
+            this.posHans.AutoSize = true;
+            this.posHans.Location = new System.Drawing.Point(1035, 229);
+            this.posHans.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.posHans.Name = "posHans";
+            this.posHans.Size = new System.Drawing.Size(69, 20);
+            this.posHans.TabIndex = 8;
+            this.posHans.Text = "Position:";
             // 
-            // label4
+            // posPeter
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(690, 208);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Position:";
+            this.posPeter.AutoSize = true;
+            this.posPeter.Location = new System.Drawing.Point(1035, 320);
+            this.posPeter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.posPeter.Name = "posPeter";
+            this.posPeter.Size = new System.Drawing.Size(69, 20);
+            this.posPeter.TabIndex = 9;
+            this.posPeter.Text = "Position:";
             // 
-            // label5
+            // posFridolin
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(690, 260);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 13);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "Position:";
+            this.posFridolin.AutoSize = true;
+            this.posFridolin.Location = new System.Drawing.Point(1035, 400);
+            this.posFridolin.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.posFridolin.Name = "posFridolin";
+            this.posFridolin.Size = new System.Drawing.Size(69, 20);
+            this.posFridolin.TabIndex = 10;
+            this.posFridolin.Text = "Position:";
             // 
-            // label6
+            // posBertha
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(690, 318);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 13);
-            this.label6.TabIndex = 11;
-            this.label6.Text = "Position:";
+            this.posBertha.AutoSize = true;
+            this.posBertha.Location = new System.Drawing.Point(1035, 489);
+            this.posBertha.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.posBertha.Name = "posBertha";
+            this.posBertha.Size = new System.Drawing.Size(69, 20);
+            this.posBertha.TabIndex = 11;
+            this.posBertha.Text = "Position:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(52, 34);
+            this.label7.Location = new System.Drawing.Point(78, 52);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.Size = new System.Drawing.Size(51, 20);
             this.label7.TabIndex = 12;
             this.label7.Text = "Ruedi";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(52, 94);
+            this.label8.Location = new System.Drawing.Point(78, 145);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.Size = new System.Drawing.Size(45, 20);
             this.label8.TabIndex = 13;
             this.label8.Text = "Fredi";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(52, 149);
+            this.label9.Location = new System.Drawing.Point(78, 229);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(32, 13);
+            this.label9.Size = new System.Drawing.Size(47, 20);
             this.label9.TabIndex = 14;
             this.label9.Text = "Hans";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(52, 208);
+            this.label10.Location = new System.Drawing.Point(78, 320);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(32, 13);
+            this.label10.Size = new System.Drawing.Size(47, 20);
             this.label10.TabIndex = 15;
             this.label10.Text = "Peter";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(52, 260);
+            this.label11.Location = new System.Drawing.Point(78, 400);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(40, 13);
+            this.label11.Size = new System.Drawing.Size(60, 20);
             this.label11.TabIndex = 16;
             this.label11.Text = "Fridolin";
             // 
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(52, 318);
+            this.label12.Location = new System.Drawing.Point(78, 489);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(38, 13);
+            this.label12.Size = new System.Drawing.Size(57, 20);
             this.label12.TabIndex = 17;
             this.label12.Text = "Bertha";
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Race
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 388);
+            this.ClientSize = new System.Drawing.Size(1179, 597);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.progressBar6);
-            this.Controls.Add(this.progressBar5);
-            this.Controls.Add(this.progressBar4);
-            this.Controls.Add(this.progressBar3);
-            this.Controls.Add(this.progressBar2);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.posBertha);
+            this.Controls.Add(this.posFridolin);
+            this.Controls.Add(this.posPeter);
+            this.Controls.Add(this.posHans);
+            this.Controls.Add(this.posFredi);
+            this.Controls.Add(this.posRuedi);
+            this.Controls.Add(this.pB_Bertha);
+            this.Controls.Add(this.pB_Fridolin);
+            this.Controls.Add(this.pB_Peter);
+            this.Controls.Add(this.pB_Hans);
+            this.Controls.Add(this.pB_Fredi);
+            this.Controls.Add(this.pB_Ruedi);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Race";
             this.Text = "Race";
             this.ResumeLayout(false);
@@ -230,23 +274,24 @@
 
         #endregion
 
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.ProgressBar progressBar2;
-        private System.Windows.Forms.ProgressBar progressBar3;
-        private System.Windows.Forms.ProgressBar progressBar4;
-        private System.Windows.Forms.ProgressBar progressBar5;
-        private System.Windows.Forms.ProgressBar progressBar6;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ProgressBar pB_Ruedi;
+        private System.Windows.Forms.ProgressBar pB_Fredi;
+        private System.Windows.Forms.ProgressBar pB_Hans;
+        private System.Windows.Forms.ProgressBar pB_Peter;
+        private System.Windows.Forms.ProgressBar pB_Fridolin;
+        private System.Windows.Forms.ProgressBar pB_Bertha;
+        private System.Windows.Forms.Label posRuedi;
+        private System.Windows.Forms.Label posFredi;
+        private System.Windows.Forms.Label posHans;
+        private System.Windows.Forms.Label posPeter;
+        private System.Windows.Forms.Label posFridolin;
+        private System.Windows.Forms.Label posBertha;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Timer timer1;
     }
 }
