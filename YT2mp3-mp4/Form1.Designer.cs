acf7b2f6178268bdac757f30de28592c87c4e6ac
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-08-29 11:47:27 +0200

    YT2mp3-mp4 1.0 publish finished

--- YT2mp3-mp4/Form1.Designer.cs
+++ YT2mp3-mp4/Form1.Designer.cs
@@ -28,13 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.pB_FileProgress = new System.Windows.Forms.ProgressBar();
+            this.B_Path = new System.Windows.Forms.Button();
+            this.B_Convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "mp4";
+            this.openFileDialog.FileName = "*.mp4";
+            this.openFileDialog.Filter = "*.mp4|";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.DefaultExt = "mp3";
+            this.saveFileDialog.FileName = "*.mp3";
+            this.saveFileDialog.Filter = "*.mp3|";
+            this.saveFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog_FileOk);
+            // 
+            // pB_FileProgress
+            // 
+            this.pB_FileProgress.Location = new System.Drawing.Point(12, 63);
+            this.pB_FileProgress.Name = "pB_FileProgress";
+            this.pB_FileProgress.Size = new System.Drawing.Size(516, 29);
+            this.pB_FileProgress.TabIndex = 0;
+            this.pB_FileProgress.Visible = false;
+            // 
+            // B_Path
+            // 
+            this.B_Path.Location = new System.Drawing.Point(12, 22);
+            this.B_Path.Name = "B_Path";
+            this.B_Path.Size = new System.Drawing.Size(75, 35);
+            this.B_Path.TabIndex = 3;
+            this.B_Path.Text = "Pick File";
+            this.B_Path.UseVisualStyleBackColor = true;
+            this.B_Path.Click += new System.EventHandler(this.B_Path_Click);
+            // 
+            // B_Convert
+            // 
+            this.B_Convert.Location = new System.Drawing.Point(595, 63);
+            this.B_Convert.Name = "B_Convert";
+            this.B_Convert.Size = new System.Drawing.Size(93, 44);
+            this.B_Convert.TabIndex = 5;
+            this.B_Convert.Text = "Convert";
+            this.B_Convert.UseVisualStyleBackColor = true;
+            this.B_Convert.Click += new System.EventHandler(this.B_Convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 132);
+            this.Controls.Add(this.B_Convert);
+            this.Controls.Add(this.B_Path);
+            this.Controls.Add(this.pB_FileProgress);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ProgressBar pB_FileProgress;
+        private System.Windows.Forms.Button B_Path;
+        private System.Windows.Forms.Button B_Convert;
     }
 }
 
--- YT2mp3-mp4/Form1.cs
+++ YT2mp3-mp4/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Xabe.FFmpeg;
 
 namespace YT2mp3_mp4
 {
@@ -15,6 +12,62 @@ namespace YT2mp3_mp4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public string openfileName = "";
+        public string savefileName = "";
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            openfileName = openFileDialog.FileName;
+            saveFileDialog.ShowDialog();
+        }
+
+        private void B_Path_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void saveFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            savefileName = saveFileDialog.FileName;
+        }
+
+        [Obsolete]
+        private async void B_Convert_Click(object sender, EventArgs e)
+        {
+            pB_FileProgress.Visible = true;
+
+            if (savefileName != "" && openfileName != "")
+            {
+                await Task.Run(() => startConvert());
+            }
+            else
+            {
+                MessageBox.Show("Kein Pfad ausgewählt");
+            }
+
+            MessageBox.Show("Fertig!");
+            pB_FileProgress.Visible = false;
+        }
+
+        [Obsolete]
+        private async Task startConvert()
+        {
+            var mediaInfo = await MediaInfo.Get(openfileName);
+            var audioStream = mediaInfo.AudioStreams.First();
+            var conversion = Conversion.New()
+            .AddStream(audioStream)
+            .SetOutput(savefileName)
+            .SetOverwriteOutput(true)
+            .UseMultiThread(true)
+            .SetPreset(ConversionPreset.UltraFast);
+            conversion.OnProgress += async (sender, args) =>
+            {
+                await Task.Run(() => (Application.OpenForms["Form1"].Invoke(new Action(() => { (Application.OpenForms["Form1"] as Form1).pB_FileProgress.Value = args.Percent; }))));
+            };
+
+            await conversion.Start();
         }
     }
 }
