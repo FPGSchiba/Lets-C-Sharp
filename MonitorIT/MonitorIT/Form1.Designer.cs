437f3b2342ba68f28709928d21b68f3eb5680d72
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-09-08 21:02:42 +0200

    MIT first connection

--- MonitorIT/MonitorIT/Form1.Designer.cs
+++ MonitorIT/MonitorIT/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dashboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dashboardToolStripMenuItem,
+            this.actionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(2352, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dashboardToolStripMenuItem
+            // 
+            this.dashboardToolStripMenuItem.Name = "dashboardToolStripMenuItem";
+            this.dashboardToolStripMenuItem.Size = new System.Drawing.Size(116, 29);
+            this.dashboardToolStripMenuItem.Text = "Dashboard";
+            // 
+            // actionsToolStripMenuItem
+            // 
+            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(87, 29);
+            this.actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(596, 239);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(707, 411);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(2352, 1180);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dashboardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- MonitorIT/MonitorIT/Form1.cs
+++ MonitorIT/MonitorIT/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +17,69 @@ namespace MonitorIT
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        private static IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5050);
+        private static Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        private static string VerisonString = "Version: 1.0";
+
+        private static bool verified = false;
+
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                // Connect the socket to the remote endpoint. Catch any errors.    
+                try
+                {
+                    // Connect to Remote EndPoint  
+                    sender.Connect(remoteEP);
+
+                    // Receive the response from the remote device.    
+                    int bytesRec = sender.Receive(bytes);
+                    if (Encoding.ASCII.GetString(bytes, 0, bytesRec) == VerisonString)
+                    {
+                        //send the VersionString for security
+                        byte[] msg = Encoding.ASCII.GetBytes(VerisonString);
+                        int bytesSent = sender.Send(msg);
+                        verified = true;
+                    }
+                    else
+                    {
+                        // Release the socket.    
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
+
+                        MessageBox.Show("A other Version is on the Server, then on the Client. Please contact you Administrator.");
+                        Environment.Exit(1);
+                    }
+                }
+                catch (ArgumentNullException ane)
+                {
+                    MessageBox.Show("ArgumentNullException : " + ane.Message);
+                }
+                catch (SocketException se)
+                {
+                    MessageBox.Show("SocketException : " + se.Message);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Unexpected exception : "+ e.Message);
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            StartClient();
         }
     }
 }
