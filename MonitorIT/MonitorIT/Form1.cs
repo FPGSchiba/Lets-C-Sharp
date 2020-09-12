0ccbac20a2db7e4a5b12a2dd712669127a1fce6b
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-09-12 20:55:49 +0200

    MIT changes

--- MonitorIT/MonitorIT/Form1.cs
+++ MonitorIT/MonitorIT/Form1.cs
@@ -19,14 +19,14 @@ namespace MonitorIT
             InitializeComponent();
         }
 
-        private static IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
-        private static IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5050);
-        private static Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-        private static string VerisonString = "Version: 1.0";
+        public static string ip = "127.0.0.1";
+        public IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse(ip), 5050);
+        public Socket socket = new Socket(IPAddress.Parse("127.0.0.1").AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        public string VerisonString = "Version: 1.0";
 
         private static bool verified = false;
 
-        public static void StartClient()
+        public void StartClient()
         {
             byte[] bytes = new byte[1024];
 
@@ -36,22 +36,22 @@ namespace MonitorIT
                 try
                 {
                     // Connect to Remote EndPoint  
-                    sender.Connect(remoteEP);
+                    socket.Connect(remoteEP);
 
                     // Receive the response from the remote device.    
-                    int bytesRec = sender.Receive(bytes);
+                    int bytesRec = socket.Receive(bytes);
                     if (Encoding.ASCII.GetString(bytes, 0, bytesRec) == VerisonString)
                     {
                         //send the VersionString for security
                         byte[] msg = Encoding.ASCII.GetBytes(VerisonString);
-                        int bytesSent = sender.Send(msg);
+                        int bytesSent = socket.Send(msg);
                         verified = true;
                     }
                     else
                     {
                         // Release the socket.    
-                        sender.Shutdown(SocketShutdown.Both);
-                        sender.Close();
+                        socket.Shutdown(SocketShutdown.Both);
+                        socket.Close();
 
                         MessageBox.Show("A other Version is on the Server, then on the Client. Please contact you Administrator.");
                         Environment.Exit(1);
@@ -60,26 +60,55 @@ namespace MonitorIT
                 catch (ArgumentNullException ane)
                 {
                     MessageBox.Show("ArgumentNullException : " + ane.Message);
+                    Environment.Exit(1);
                 }
                 catch (SocketException se)
                 {
                     MessageBox.Show("SocketException : " + se.Message);
+                    Environment.Exit(1);
                 }
                 catch (Exception e)
                 {
                     MessageBox.Show("Unexpected exception : "+ e.Message);
+                    Environment.Exit(1);
                 }
 
             }
             catch (Exception e)
             {
                 MessageBox.Show(e.Message);
+                Environment.Exit(1);
             }
         }
 
         private void Form1_Shown(object sender, EventArgs e)
         {
             StartClient();
+            fill_CPU();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            fill_CPU();
+        }
+
+        private void fill_CPU()
+        {
+            if (verified)
+            {
+                byte[] msg = Encoding.ASCII.GetBytes("GetCPU");
+                byte[] rec = new byte[1024];
+
+                int byteSent = socket.Send(msg);
+                int byteRec = socket.Receive(rec);
+
+                cr_CPU.Series["CPU Percent"].Points.AddXY(DateTime.Now.ToString("h:mm:ss"), Encoding.ASCII.GetString(rec, 0, byteRec));
+                if (cr_CPU.Series["CPU Percent"].Points.Count() >= 10)
+                {
+                    cr_CPU.Series["CPU Percent"].Points.RemoveAt(0);
+                    cr_CPU.ResetAutoValues();
+                }
+            }
         }
     }
 }
