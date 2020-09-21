e9abb6b5b52153be91f9508b59da3238e7a54725
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2020-09-21 17:23:59 +0200

    BankHeLa20 created

---
+++ AktienBank/Form1.cs
@@ -0,0 +1,82 @@
+﻿using Pferderennen;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AktienBank
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Mohatu;
+        int Uru;
+        int Nuka;
+        int Chumvi;
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Mohatu += getRandom();
+            Uru += getRandom();
+            Nuka += getRandom();
+            Chumvi += getRandom();
+
+            if(Mohatu <= 0)
+            {
+                Mohatu = 0;
+            }
+            if(Uru <= 0)
+            {
+                Uru = 0;
+            }
+            if(Nuka <= 0)
+            {
+                Nuka = 0;
+            }
+            if(Chumvi <= 0)
+            {
+                Chumvi = 0;
+            }
+
+            chart1.Series[0].Points.AddXY(DateTime.Now.ToString("h:mm:ss"), Mohatu);
+            chart1.Series[1].Points.AddXY(DateTime.Now.ToString("h:mm:ss"), Uru);
+            chart1.Series[2].Points.AddXY(DateTime.Now.ToString("h:mm:ss"), Nuka);
+            chart1.Series[3].Points.AddXY(DateTime.Now.ToString("h:mm:ss"), Chumvi);
+
+            if(chart1.Series[0].Points.Count == 10)
+            {
+                chart1.Series[0].Points.RemoveAt(0);
+                chart1.Series[1].Points.RemoveAt(0);
+                chart1.Series[2].Points.RemoveAt(0);
+                chart1.Series[3].Points.RemoveAt(0);
+            }
+        }
+
+        private int getRandom()
+        {
+            return IntExtension.Between(-20, 20);
+        }
+
+        private int startProg()
+        {
+            return IntExtension.Between(10, 90);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Mohatu = startProg();
+            Uru = startProg();
+            Nuka = startProg();
+            Chumvi = startProg();
+        }
+    }
+}
---
+++ AktienBank/IntExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pferderennen
+{
+    static class IntExtension
+    {
+        public static int LimitToRange(this int value, int inclusiveMinimum, int inclusiveMaximum)
+        {
+            if (value < inclusiveMinimum) { return inclusiveMinimum; }
+            if (value > inclusiveMaximum) { return inclusiveMaximum; }
+            return value;
+        }
+
+        private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
+
+        public static int Between(int minimumValue, int maximumValue)
+        {
+            byte[] randomNumber = new byte[1];
+
+            _generator.GetBytes(randomNumber);
+
+            double asciiValueOfRandomCharacter = Convert.ToDouble(randomNumber[0]);
+
+            // We are using Math.Max, and substracting 0.00000000001, 
+            // to ensure "multiplier" will always be between 0.0 and .99999999999
+            // Otherwise, it's possible for it to be "1", which causes problems in our rounding.
+            double multiplier = Math.Max(0, (asciiValueOfRandomCharacter / 255d) - 0.00000000001d);
+
+            // We need to add one to the range, to allow for the rounding done with Math.Floor
+            int range = maximumValue - minimumValue + 1;
+
+            double randomValueInRange = Math.Floor(multiplier * range);
+
+            return (int)(minimumValue + randomValueInRange);
+        }
+    }
+
+    public sealed class ReverseComparer<T> : IComparer<T>
+    {
+        private readonly IComparer<T> original;
+
+        public ReverseComparer(IComparer<T> original)
+        {
+            // TODO: Validation
+            this.original = original;
+        }
+
+        public int Compare(T left, T right)
+        {
+            return original.Compare(right, left);
+        }
+    }
+}
