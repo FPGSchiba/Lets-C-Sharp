80c8073999aaa4d6b2b03d6e1f3e3db04409cb7b
FPGSchiba <59923683+FPGSchiba@users.noreply.github.com>
2021-03-29 16:18:56 +0200

    tests

--- SSLClient/SSLTryForSekaiju/SslTcpClient.cs
+++ SSLClient/SSLTryForSekaiju/SslTcpClient.cs
@@ -30,7 +30,7 @@ namespace SSLTryForSekaiju
 
             private static void RunClient(string machineName, string serverName)
             {
-                TcpClient client = new TcpClient(machieName,14271);
+                TcpClient client = new TcpClient(machineName,14271);
                 Console.WriteLine("Client connected.");
                 SslStream sslStream = new SslStream(
                     client.GetStream(),
